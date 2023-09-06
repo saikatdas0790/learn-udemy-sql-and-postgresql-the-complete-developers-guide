320a142a516d8b126dd04b9857b56138bc600095
Saikat Das <saikatdas0790@gmail.com>
2023-09-06 13:45:33 +0530

    7. Inserting Data Into a Table

---
+++ 1. Simple - But Powerful - SQL Statements/7.1 005 - sql.sql
@@ -0,0 +1,7 @@
+insert into cities (name, country, population, area)
+values ('Tokyo', 'Japan', 38505000, 8223);
+
+insert into cities (name, country, population, area)
+values ('Delhi', 'India', 28125000, 2240),
+  ('Shanghai', 'China', 22125000, 4015),
+  ('Sao Paulo', 'Brazil', 20935000, 3043);
