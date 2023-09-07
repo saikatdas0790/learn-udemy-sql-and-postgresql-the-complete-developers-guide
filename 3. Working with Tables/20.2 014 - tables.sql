b7c3785e58b1b69bec8d50738ecf077315c61351
Saikat Das <saikatdas0790@gmail.com>
2023-09-07 21:45:09 +0530

    20. Testing Deletion Constraints

---
+++ 3. Working with Tables/20.2 014 - tables.sql
@@ -0,0 +1,26 @@
+drop table photos;
+
+-- This will result in an error
+select *
+from photos;
+
+create table photos (
+  id SERIAL primary key,
+  url VARCHAR(200),
+  user_id INTEGER references users(id) on delete cascade
+);
+
+insert into photos (url, user_id)
+values ('http://one.jpg', 4),
+  ('http://two.jpg', 1),
+  ('http://25.jpg', 1),
+  ('http://36.jpg', 1),
+  ('http://754.jpg', 2),
+  ('http://35.jpg', 3),
+  ('http://256.jpg', 4);
+
+delete from users
+where id = 1;
+
+select *
+from photos;
